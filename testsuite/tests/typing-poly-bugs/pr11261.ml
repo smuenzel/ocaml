cdb5b3435348af2d64afd5bae9bafdd847cd6971
Stefan Muenzel <source@s.muenzel.net>
2022-07-26 21:54:57 +0800

    expand test for pr1161

--- testsuite/tests/typing-poly-bugs/pr11261.ml
+++ testsuite/tests/typing-poly-bugs/pr11261.ml
@@ -17,3 +17,17 @@ Line 3, characters 21-25:
                          ^^^^
 Error: The type 'b t does not expand to a polymorphic variant type
 |}]
+
+type 'a tt = [`F]
+
+type 'a second = F : ('b#tt as 'a) second
+
+
+[%%expect{|
+type 'a tt = [ `F ]
+Line 3, characters 22-27:
+3 | type 'a second = F : ('b#tt as 'a) second
+                          ^^^^^
+Alert deprecated: old syntax for polymorphic variant type
+type 'a second = F : 'b tt second
+|}]
