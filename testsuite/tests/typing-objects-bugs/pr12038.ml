de363a756a79a417def0af78cc3062b847adbea2
Leo White <leo@lpw25.net>
2023-03-30 12:22:51 +0100

    Don't assert when a private method is prematurely absent
    This can happen when an object type has been explicitly closed by
    the user.

--- testsuite/tests/typing-objects-bugs/pr12038.ml
+++ testsuite/tests/typing-objects-bugs/pr12038.ml
@@ -98,6 +98,9 @@ let o = object(_ : 'self)
   method m = ()
 end
 [%%expect{|
-Uncaught exception: File "typing/ctype.ml", line 3636, characters 29-35: Assertion failed
-
+Line 4, characters 2-15:
+4 |   method m = ()
+      ^^^^^^^^^^^^^
+Error: This object is expected to have type : "<  >"
+       This type does not have a method "m".
 |}]
