2d7e43ccb065c73916cf83eccb73fd0ae8cb3f83
Stefan Muenzel <source@s.muenzel.net>
2026-03-17 21:00:07 +0100

    Expand types for printing errors in typecore

--- testsuite/tests/typing-objects/Tests.ml
+++ testsuite/tests/typing-objects/Tests.ml
@@ -1336,7 +1336,7 @@ class c : object method private test : unit end
 Line 6, characters 9-16:
 6 | let () = (new c)#test
              ^^^^^^^
-Error: This expression has type "c"
+Error: This expression has type "c" = "<  >"
        It has no method "test"
 |}];;
 
@@ -1366,7 +1366,7 @@ class c : object method private test : unit end
 Line 10, characters 9-16:
 10 | let () = (new c)#test
               ^^^^^^^
-Error: This expression has type "c"
+Error: This expression has type "c" = "<  >"
        It has no method "test"
 |}];;
 
