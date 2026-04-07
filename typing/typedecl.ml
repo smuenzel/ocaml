420c4c5b8c57880f0627d2b20884866de1869034
Stefan Muenzel <source@s.muenzel.net>
2026-04-07 06:38:18 +0200

    Remove stale comment

--- typing/typedecl.ml
+++ typing/typedecl.ml
@@ -1038,17 +1038,6 @@ let is_reachable
    Note: we check that the types at the toplevel of the declaration
    are not reachable unguarded from themselves, that is, we check that
    there is no cycle going through the "root" of the declaration.
-
-   (* CR smuenzel: the below is no longer true. *)
-
-   But we *also* check that all the type sub-expressions reachable from
-   the root even those that are guarded, are themselves
-   well-founded. (So we check the absence of cycles, even for cycles
-   going through inner type subexpressions but not the root.
-
-   We are not actually sure that this "deep check" is necessary
-   (we don't have an example at hand where it is necessary), but we
-   are doing it anyway out of caution.
 *)
 let check_well_founded_decl
     ~abs_env ~final_env ~is_decl_path loc path decl =
